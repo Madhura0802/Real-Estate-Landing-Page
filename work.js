b387d6921cf2d7c12deb81013fabdfc780bb369c
Madhura0802 <102659752+Madhura0802@users.noreply.github.com>
2024-03-29 15:12:11 +0530

    Add files via upload

---
+++ work.js
@@ -0,0 +1,13 @@
+
+
+function confirmation()
+{
+    if(confirm("Do you want to submit form?")==true)
+    {
+        alert( "Sucessfully Submitted!");
+    }
+    else{
+     alert( "Something went wrong...");
+    }
+}
+
